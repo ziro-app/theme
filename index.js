23a10c710e7fa402c384c2bebbdf692774c2bf3d
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-12-26 16:14:55 -0800

    export theme file

---
+++ index.js
@@ -0,0 +1,50 @@
+module.exports = {
+
+	/* STANDARD CONTAINERS */
+	container: {
+		maxWidth: '500px',
+		height: '100vh',
+		boxSizing: 'border-box',
+		margin: '0 auto'
+	},
+	containerWithPadding: {
+		maxWidth: '500px',
+		height: '100vh',
+		boxSizing: 'border-box',
+		margin: '0 auto',
+		padding: '20px 5% 0'
+	},
+	padding: '20px 5% 0',
+
+	/* COLORS */
+	primaryColor: '#222222', // rgb(34,34,34)
+	primaryAltColor: '#303E4F',
+	primaryAltColorLight1: '#4F637B',
+	primaryAltColorLight2: '#73869E',
+	grayColor1: '#828282',
+	grayColor2: '#BDBDBD',
+	grayColor3: '#E0E0E0',
+	grayColor4: '#F2F2F2',
+	secondaryColor: '#FFE401',
+	successColor: '#4BCA81',
+	alertColor: '#EB5757',
+	warningColor: '#F7BA00',
+	otherColor: '#2D9CDB',
+
+	/* FONTS */
+	fontTitle: 'Rubik',
+	fontBody: 'Work Sans',
+	fontWeightTitle: '500',
+	fontWeightBody: '400',
+	fontWeightMuted: '300',
+	fontSizeNormal: '1.6rem',
+	fontSizeInput: '1.5rem',
+	fontSizeSmall: '1.3rem',
+
+	/* GRADIENT */
+	gradient: `linear-gradient(#3A3A3A 10%, #333 30%, #222 60%, #000)`,
+
+	/* SHADOW */
+	shadow: `0px 5px 15px -4px rgba(34,34,34,0.45), 0px 5px 15px -4px rgba(34,34,34,0.40), 
+		0px 5px 15px -4px rgba(34,34,34,0.35), 0px 5px 15px -4px rgba(34,34,34,0.30)`
+}
